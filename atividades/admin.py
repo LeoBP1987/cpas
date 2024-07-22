18071d604305692f120098583b57c80575b569f1
Leonardo <leonardobp1987@gmail.com>
2024-07-22 18:00:15 -0300

    Configurando BD Atividades

--- atividades/admin.py
+++ atividades/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from atividades.models import TipoAtividade, Instituicao, Atividades
 
-# Register your models here.
+class ListandoTipoAtividade(admin.ModelAdmin):
+    list_display = ('id', 'nome_tipo', 'horas')
+    list_display_links = ('id', 'nome_tipo')
+    search_fields = ('nome_tipo', 'horas')
+    list_filter = ('horas',)
+    list_per_page = 10
+
+class ListandoInstituicao(admin.ModelAdmin):
+    list_display = ('id', 'nome_inst', 'valor_padrao')
+    list_display_links = ('id', 'nome_inst',)
+    search_fields = ('nome_inst', 'valor_padrao')
+    list_filter = ('nome_inst', 'valor_padrao')
+    list_per_page = 10
+
+class ListandoAtividades(admin.ModelAdmin):
+    list_display = ('id', 'tipo_atividade', 'data', 'valor', 'sequencia')
+    list_display_links = ('id', 'data')
+    search_fields = ('instituicao', 'data', 'valor')
+    list_filter = ('data', 'valor')
+    list_per_page = 10
+
+admin.site.register(TipoAtividade, ListandoTipoAtividade)
+admin.site.register(Instituicao, ListandoInstituicao)
+admin.site.register(Atividades, ListandoAtividades)
--- atividades/models.py
+++ atividades/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class TipoAtividade(models.Model):
+    nome_tipo = models.CharField(max_length=30, null=False, blank=False)
+    horas = models.IntegerField()
+
+class Instituicao(models.Model):
+    nome_inst = models.CharField(max_length=33, null=False, blank=False)
+    imagem = models.ImageField(upload_to='intituicao/%Y/%m/%d', blank=True)
+    valor_padrao = models.DecimalField(max_digits=10, decimal_places=2)
+    endereco = models.CharField(max_length=150,)
+    telefone = models.CharField(max_length=12,)
+    contato = models.CharField(max_length=30,)
+
+class Atividades(models.Model):
+    intituicao = models.ForeignKey(
+        to=Instituicao,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='atividades'
+    )
+    tipo_atividade = models.ForeignKey(
+        to=TipoAtividade,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='tipo'
+    )
+    data = models.DateTimeField()
+    entrada = models.TimeField()
+    saida = models.TimeField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    sequencia = models.IntegerField()
+    data_final_seq = models.DateTimeField()
